7ce81bc595abdaa82d587c194f5c8e07902a7fc0
ndizeyedavid <davidndizeye101@gmail.com>
2025-09-13 14:56:38 +0200

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const fontSans = FontSans({
@@ -61,6 +62,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Analytics />
         <ThemeProvider attribute="class" defaultTheme="dark">
           <TooltipProvider delayDuration={0}>
             {children}
